7380c66fe107201bad9883af9da465129215c7cb
MedicArwen <45285123+MedicArwen@users.noreply.github.com>
2021-06-23 09:01:20 +0200

    ajout de propriétés
    Remplacement des attributs par des getters / setters sur la classe Hero

--- src/app/business/Hero.ts
+++ src/app/business/Hero.ts
@@ -2,25 +2,57 @@ import { ThrowStmt } from "@angular/compiler";
 
 export class Hero
 {
-    id:number;
-    pseudo:string;
-    nom:string;
-    prenom:string;
-    estSuperVilain:boolean;
-    image:string;
-    shortText:string;
-    longText:String;
+    private _id:number;
+    private _pseudo:string;
+    private _nom:string;
+    private _prenom:string;
+    private _estSuperVilain:boolean;
+    private _image:string;
+    private _shortText:string;
+    private _longText:string;
+    get id():number
+    {
+        return this._id;
+    }
+    get pseudo():string
+    {
+        return this._pseudo;
+    }
+    get nom():string
+    {
+        return this._nom;
+    }
+    get prenom():string
+    {
+        return this._prenom;
+    }
+    get estSuperVilain():boolean
+    {
+        return this._estSuperVilain;
+    }
+    get image():string
+    {
+        return 'assets/img/'+this._image;
+    }
+    get shortText():string
+    {
+        return this._shortText;
+    }
+    get longText():string
+    {
+        return this._longText;
+    }
 
     constructor (pID:number,pPseudo:string,pNom:string,pPrenom:string,pEstSuperVilain:boolean,
         pImage:string,pShortText:string,pLongText:string)
     {
-        this.id = pID;
-        this.prenom = pPrenom;
-        this.nom = pNom;
-        this.pseudo = pPseudo;
-        this.estSuperVilain = pEstSuperVilain;
-        this.image = pImage;
-        this.shortText = pShortText;
-        this.longText = pLongText;
+        this._id = pID;
+        this._prenom = pPrenom;
+        this._nom = pNom;
+        this._pseudo = pPseudo;
+        this._estSuperVilain = pEstSuperVilain;
+        this._image = pImage;
+        this._shortText = pShortText;
+        this._longText = pLongText;
     }
 }
--- src/app/business/MockHeroes.ts
+++ src/app/business/MockHeroes.ts
@@ -1,74 +1,74 @@
 import { Hero } from "./Hero";
 
 export const HEROES: Hero[] = [
-    {
-        id:1,
-        pseudo:'SuperMan',
-        prenom:'Clark',
-        nom:'Kent',
-        estSuperVilain:false,
-        image:'assets/img/superman.jpg',
-        shortText:'Super Man, un alien venu d\'ailleurs',
-        longText:'Super Man, un alien venu d\'ailleurs. Un orphelin de krypton'
-    },
-    {
-        id:2,
-        pseudo:'Batman',
-        prenom:'Bruce',
-        nom:'Wayne',
-        estSuperVilain:false,
-        image:'assets/img/batman.jpg',
-        shortText:'Un milliardaire de Gotham qui se déguise en chauve souris',
-        longText:'Un milliardaire de Gotham qui se déguise en chauve souris. Train limite parfois côté moralité. Il a même tué superman quelque part...'
-    },
-    {
-        id:3,
-        pseudo:'Captain America',
-        prenom:'Steeve',
-        nom:'Rogers',
-        estSuperVilain:false,
-        image:'assets/img/captain_america.jpg',
-        shortText:'Un faible homme qui voulait être assez fort pour rejoindre l\'armée américaine ',
-        longText:'Un faible homme qui voulait être assez fort pour rejoindre l\'armée américaine. Il a fini congelé et traine ses guêtres bien après la seconde guerre mondiale.'
-    },
-    {
-        id:4,
-        pseudo:'Black Widow',
-        prenom:'Natasha',
-        nom:'Romanoff',
-        estSuperVilain:false,
-        image:'assets/img/black_widow.jpg',
-        shortText:'Une espionne russe qui en remonterait à James Bond',
-        longText:'Une espionne russe qui en remonterait à James Bond. On aurait pu la croire vilaine, mais elle aide les gentils !'
-    },
-    {
-        id:5,
-        pseudo:'Wonder Woman',
-        prenom:'Diana',
-        nom:'Prince',
-        estSuperVilain:false,
-        image:'assets/img/wonder_woman.jpg',
-        shortText:'Une amazone sortie de sa cambrousse',
-        longText:'Une amazone sortie de sa cambrousse. Un brin idéaliste avant de perdre son amour de pilote pendant la premiere guerre mondiale.'
-    },
-    {
-        id:6,
-        pseudo:'Mystique',
-        prenom:'Raven',
-        nom:'Darkholme',
-        estSuperVilain:true,
-        image:'assets/img/mystic.jpg',
-        shortText:'Une métamorphe qui aime bien mettre le souk',
-        longText:'Une métamorphe qui aime bien mettre le souk, bleue des pieds à la tête.' 
-    },  
-    {
-        id:7,
-        pseudo:'Le Joker',
-        prenom:'Jack',
-        nom:'Napier',
-        estSuperVilain:true,
-        image:'assets/img/le_joker.jpg',
-        shortText:'Un ancien mafieu tombé dans dans de l\'acide et devenu timbré',
-        longText:'Un ancien mafieu tombé dans dans de l\'acide et devenu timbré. Il a même rendu folle son psy, Harley...'
-    },
+    new Hero(
+        1,
+        'SuperMan',
+        'Clark',
+        'Kent',
+        false,
+        'superman.jpg',
+        'Super Man, un alien venu d\'ailleurs',
+        'Super Man, un alien venu d\'ailleurs. Un orphelin de krypton'
+    ),
+    new Hero(
+        2,
+        'Batman',
+        'Bruce',
+        'Wayne',
+        false,
+        'batman.jpg',
+        'Un milliardaire de Gotham qui se déguise en chauve souris',
+        'Un milliardaire de Gotham qui se déguise en chauve souris. Train limite parfois côté moralité. Il a même tué superman quelque part...'
+    ),
+    new Hero(
+        3,
+        'Captain America',
+        'Steeve',
+        'Rogers',
+        false,
+        'captain_america.jpg',
+        'Un faible homme qui voulait être assez fort pour rejoindre l\'armée américaine ',
+        'Un faible homme qui voulait être assez fort pour rejoindre l\'armée américaine. Il a fini congelé et traine ses guêtres bien après la seconde guerre mondiale.'
+    ),
+    new Hero(
+        4,
+        'Black Widow',
+        'Natasha',
+        'Romanoff',
+        false,
+        'black_widow.jpg',
+        'Une espionne russe qui en remonterait à James Bond',
+        'Une espionne russe qui en remonterait à James Bond. On aurait pu la croire vilaine, mais elle aide les gentils !'
+    ),
+    new Hero(
+        5,
+        'Wonder Woman',
+        'Diana',
+        'Prince',
+        false,
+        'wonder_woman.jpg',
+        'Une amazone sortie de sa cambrousse',
+        'Une amazone sortie de sa cambrousse. Un brin idéaliste avant de perdre son amour de pilote pendant la premiere guerre mondiale.'
+    ),
+    new Hero(
+        6,
+        'Mystique',
+        'Raven',
+        'Darkholme',
+        true,
+        'mystic.jpg',
+        'Une métamorphe qui aime bien mettre le souk',
+        'Une métamorphe qui aime bien mettre le souk, bleue des pieds à la tête.' 
+    ),  
+    new Hero(
+        7,
+        'Le Joker',
+        'Jack',
+        'Napier',
+        true,
+        'le_joker.jpg',
+        'Un ancien mafieu tombé dans dans de l\'acide et devenu timbré',
+        'Un ancien mafieu tombé dans dans de l\'acide et devenu timbré. Il a même rendu folle son psy, Harley...'
+    )
   ];
